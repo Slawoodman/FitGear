fa3c6285c3806266d78984be78538d256d532d46
Slawoodman <Slawoodmen@gmail.com>
2024-04-11 13:59:29 +0200

    renamed:    OrderManager/__init__.py -> FitGear/__init__.py 	renamed:    OrderManager/__pycache__/__init__.cpython-310.pyc -> FitGear/__pycache__/__init__.cpython-310.pyc 	renamed:    OrderManager/__pycache__/settings.cpython-310.pyc -> FitGear/__pycache__/settings.cpython-310.pyc 	renamed:    OrderManager/__pycache__/urls.cpython-310.pyc -> FitGear/__pycache__/urls.cpython-310.pyc 	new file:   FitGear/__pycache__/wsgi.cpython-310.pyc 	renamed:    OrderManager/asgi.py -> FitGear/asgi.py 	renamed:    OrderManager/settings.py -> FitGear/settings.py 	renamed:    OrderManager/urls.py -> FitGear/urls.py 	renamed:    OrderManager/wsgi.py -> FitGear/wsgi.py 	deleted:    OrderManager/__pycache__/wsgi.cpython-310.pyc 	modified:   manage.py

--- FitGear/wsgi.py
+++ FitGear/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for OrderManager project.
+WSGI config for FitGear project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OrderManager.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FitGear.settings")
 
 application = get_wsgi_application()

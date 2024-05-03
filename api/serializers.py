7c7eb920bdf13c3448197fa9597db1077272394a
Slawoodman <Slawoodmen@gmail.com>
2024-05-04 00:07:57 +0200

    modified:   api/__pycache__/serializers.cpython-310.pyc 	modified:   api/serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from market.models import Product, OrderItem
+from market.models import Product, OrderItem, Order, Cart, CartItem
 from users.models import User
 
 
@@ -15,10 +15,35 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "first_name", "last_name", "email"]
 
 
+class CartItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(many=False)
+
+    class Meta:
+        model = CartItem
+        fields = "__all__"
+
+
+class CartSerializer(serializers.ModelSerializer):
+    customer = UserSerializer(many=False)
+    cart_items = CartItemSerializer(many=True)
+
+    class Meta:
+        model = Cart
+        fields = "__all__"
+
+
 class OrderItemSerializer(serializers.ModelSerializer):
     product = ProductSerializer(many=False)
-    customer = UserSerializer(many=False)
 
     class Meta:
         model = OrderItem
+        fields = "__all__"
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    customer = UserSerializer(many=False)
+    order_items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
         fields = "__all__"
